784110ea931988754b86312f55e6b43cf3194233
ruairidht <tay107318@bksb.co.uk>
2022-04-22 15:47:49 +0100

    moved tofixed to end instead after each number

--- taxCalc.js
+++ taxCalc.js
@@ -8,47 +8,43 @@
 // Over £750,000	      = 12%
 
 const readline = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 readline.question(
-    `\nWhat's the value of the property? (£): `,
-    (propertyPrice) => {
-        var tax = calculateTax(propertyPrice);
-        console.log(`Land and Buildings Transaction Tax to pay is £${tax}\n`);
-        readline.close();
-    }
+  `\nWhat's the value of the property? (£): `,
+  (propertyPrice) => {
+    var tax = calculateTax(propertyPrice);
+    console.log(`Land and Buildings Transaction Tax to pay is £${tax}\n`);
+    readline.close();
+  }
 );
 
 function calculateTax(propertyPrice) {
-    var tax = null;
-    propertyPrice = parseFloat(propertyPrice);
+  var tax = null;
+  propertyPrice = parseFloat(propertyPrice);
 
-    if (propertyPrice > 750000) {
-        tax = (
-            (propertyPrice - 750000) * 0.12 +
-            (750000 - 325001) * 0.1 +
-            (325000 - 250001) * 0.05 +
-            (250000 - 145001) * 0.02
-        ).toFixed(2);
-    } else if (propertyPrice > 325001) {
-        tax = (
-            (propertyPrice - 325001) * 0.1 +
-            (325000 - 250001) * 0.05 +
-            (250000 - 145001) * 0.02
-        ).toFixed(2);
-    } else if (propertyPrice > 250001) {
-        tax = ((propertyPrice - 250001) * 0.05 + (250000 - 145001) * 0.02).toFixed(
-            2
-        );
-    } else if (propertyPrice > 145001) {
-        tax = ((propertyPrice - 145001) * 0.02).toFixed(2);
-    } else if (propertyPrice >= 0) {
-        tax = 0;
-    }
+  if (propertyPrice > 750000) {
+    tax =
+      (propertyPrice - 750000) * 0.12 +
+      (750000 - 325001) * 0.1 +
+      (325000 - 250001) * 0.05 +
+      (250000 - 145001) * 0.02;
+  } else if (propertyPrice > 325001) {
+    tax =
+      (propertyPrice - 325001) * 0.1 +
+      (325000 - 250001) * 0.05 +
+      (250000 - 145001) * 0.02;
+  } else if (propertyPrice > 250001) {
+    tax = (propertyPrice - 250001) * 0.05 + (250000 - 145001) * 0.02;
+  } else if (propertyPrice > 145001) {
+    tax = (propertyPrice - 145001) * 0.02;
+  } else if (propertyPrice >= 0) {
+    tax = 0;
+  }
 
-    return tax == null ? -1 : parseFloat(tax);
+  return tax == null ? -1 : parseFloat(tax.toFixed(2));
 }
 
 module.exports = calculateTax;
